b6d8d66c57338af32ccd611b4484b650288ef70c
Parker Selbert <parker@sorentwo.com>
2019-06-13 17:52:47 -0500

    Small style and dependency updates

--- mix.exs
+++ mix.exs
@@ -1,15 +1,29 @@
 defmodule ObanWeb.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :oban_web,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+
+      description: "Oban Web Component",
+
+      # Docs
+      name: "ObanWeb",
+      docs: [
+        main: "ObanWeb",
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/sorentwo/oban_web",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -22,15 +36,24 @@ defmodule ObanWeb.MixProject do
     ]
   end
 
+  def package do
+    [
+      organization: "oban",
+      licenses: ["Commercial"],
+      links: []
+    ]
+  end
+
   defp deps do
     [
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:oban, ">= 0.0.0", path: "../oban"},
+      {:oban, "~> 0.4"},
       {:phoenix, "~> 1.4"},
       {:phoenix_html, "~> 2.13"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
-      {:phoenix_pubsub, "~> 1.1"}
+      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
