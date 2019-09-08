cc9d6d08517d4ccb7cb96af8ee30cbb79caf9a24
Parker Selbert <parker@sorentwo.com>
2019-09-08 10:41:26 -0500

    Remove the unnecessary dashboard controller

--- lib/oban_web/controllers/dashboard_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule ObanWeb.DashboardController do
-  use ObanWeb, :controller
-
-  import Phoenix.LiveView.Controller, only: [live_render: 3]
-
-  def index(conn, _params) do
-    live_render(conn, ObanWeb.DashboardLive, session: %{})
-  end
-end
