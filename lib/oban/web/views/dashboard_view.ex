c07bf2ce9e76d30c47184fc9de58ace581c97524
Parker Selbert <parker@sorentwo.com>
2020-12-30 09:10:32 -0600

    Remove unused dashboard view helper functions

--- lib/oban/web/views/dashboard_view.ex
+++ lib/oban/web/views/dashboard_view.ex
@@ -1,19 +1,3 @@
 defmodule Oban.Web.DashboardView do
   use Oban.Web, :view
-
-  alias Oban.Web.IconView
-
-  @doc """
-  A helper for rendering icon templates from the IconView.
-  """
-  def icon(name) do
-    render(IconView, name <> ".html")
-  end
-
-  @clearable_filter_types [:node, :queue, :worker]
-  def clearable_filters(filters) do
-    for {type, name} <- filters, type in @clearable_filter_types, name != "any" do
-      {to_string(type), name}
-    end
-  end
 end
--- lib/oban/web/views/icon_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule Oban.Web.IconView do
-  use Oban.Web, :view
-end
