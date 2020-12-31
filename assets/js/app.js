425eeedbe47ce3dc1c5757b4080003d3e72a1e7e
Parker Selbert <parker@sorentwo.com>
2020-12-31 14:38:41 -0600

    Add data-loading messages to action buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 import css from "../css/app.css"
 
-import "../../deps/phoenix_html/priv/static/phoenix_html"
-import {Socket, LongPoll} from "../../deps/phoenix/priv/static/phoenix"
-import {LiveSocket} from "../../deps/phoenix_live_view/priv/static/phoenix_live_view"
+import "phoenix_html"
+import {Socket, LongPoll} from "phoenix"
+import {LiveSocket} from "phoenix_live_view"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 const transport = document.querySelector("meta[name='live-transport']").getAttribute("content");
--- lib/oban/web/components/detail_component.ex
+++ lib/oban/web/components/detail_component.ex
@@ -24,7 +24,9 @@ defmodule Oban.Web.DetailComponent do
           <%= if can?(:cancel_jobs, @access) and cancelable?(@job) do %>
             <a href="#"
                class="group flex items-center ml-4 text-sm text-gray-600 bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none hover:text-yellow-600 hover:border-yellow-600"
-               phx-target="<%= @myself %>" phx-click="cancel">
+               data-disable-with="Cancelling…"
+               phx-target="<%= @myself %>"
+               phx-click="cancel">
               <svg class="-ml-1 mr-1 h-5 w-5 text-gray-500 group-hover:text-yellow-500" fill="currentColor" viewBox="0 0 20 20"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
               Cancel
             </a>
@@ -33,7 +35,9 @@ defmodule Oban.Web.DetailComponent do
           <%= if can?(:retry_jobs, @access) and runnable?(@job) do %>
             <a href="#"
                class="group flex items-center ml-3 text-sm text-gray-600 bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none hover:text-blue-600 hover:border-blue-600"
-               phx-target="<%= @myself %>" phx-click="retry">
+               data-disable-with="Running…"
+               phx-target="<%= @myself %>"
+               phx-click="retry">
               <svg class="-ml-1 mr-1 h-5 w-5 text-gray-500 group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20"><path d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
               Run Now
             </a>
@@ -42,7 +46,9 @@ defmodule Oban.Web.DetailComponent do
           <%= if can?(:retry_jobs, @access) and retryable?(@job) do %>
             <a href="#"
                class="group flex items-center ml-3 text-sm text-gray-600 bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none hover:text-blue-600 hover:border-blue-600"
-               phx-target="<%= @myself %>" phx-click="retry">
+               data-disable-with="Retrying…"
+               phx-target="<%= @myself %>"
+               phx-click="retry">
               <svg class="-ml-1 mr-1 h-5 w-5 text-gray-500 group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20"><path d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
               Retry
             </a>
@@ -52,6 +58,7 @@ defmodule Oban.Web.DetailComponent do
             <a href="#"
                class="group flex items-center ml-3 text-sm text-gray-600 bg-white px-4 py-2 border border-gray-300 rounded-md focus:outline-none hover:text-red-600 hover:border-red-600"
                data-confirm="Are you sure you want to delete this job?"
+               data-disable-with="Deleting…"
                phx-target="<%= @myself %>"
                phx-click="delete">
               <svg class="-ml-1 mr-1 h-5 w-5 text-gray-500 group-hover:text-red-600" fill="currentColor" viewBox="0 0 20 20"><path d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
