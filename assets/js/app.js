194df07bea6607858ee0c599fa23f9a3a60f058d
Parker Selbert <parker@sorentwo.com>
2021-05-12 20:44:35 -0500

    Display NProgress bar for page loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
 import css from "../css/app.css"
+import NProgress from "nprogress"
 
 import "phoenix_html"
 import {Socket, LongPoll} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
+
+window.addEventListener("phx:page-loading-start", info => NProgress.start())
+window.addEventListener("phx:page-loading-stop", info => NProgress.done())
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 const transport = document.querySelector("meta[name='live-transport']").getAttribute("content");
