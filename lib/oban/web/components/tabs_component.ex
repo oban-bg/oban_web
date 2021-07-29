27fb8a86fd38786f57e419895df3252a4af5cb84
Parker Selbert <parker@sorentwo.com>
2021-07-29 09:02:50 -0500

    Use live_redirect for tabs to prevent remounts

--- lib/oban/web/components/tabs_component.ex
+++ lib/oban/web/components/tabs_component.ex
@@ -1,12 +1,17 @@
 defmodule Oban.Web.TabsComponent do
   use Oban.Web, :live_component
 
+  @base "text-gray-300 hover:text-gray-100 px-3 py-2 font-medium text-sm rounded-md"
+
   def render(assigns) do
     ~L"""
     <nav class="ml-8 flex space-x-2">
-      <a href="<%= oban_path(@socket, :jobs) %>" phx-link="patch" class="text-gray-300 hover:text-gray-100 <%= if @page == :jobs do %>bg-blue-300 bg-opacity-25 <% end %>px-3 py-2 font-medium text-sm rounded-md">Jobs</a>
-      <a href="<%= oban_path(@socket, :queues) %>" phx-link="patch" class="text-gray-300 hover:text-gray-100 <%= if @page == :queues do %>bg-blue-300 bg-opacity-25 <% end %>px-3 py-2 font-medium text-sm rounded-md">Queues</a>
+      <%= live_redirect "Jobs", to: oban_path(@socket, :jobs), class: link_class(@page, :jobs) %>
+      <%= live_redirect "Queues", to: oban_path(@socket, :queues), class: link_class(@page, :queues) %>
     </nav>
     """
   end
+
+  defp link_class(page, page), do: @base <> " bg-blue-300 bg-opacity-25"
+  defp link_class(_pag, _exp), do: @base
 end
