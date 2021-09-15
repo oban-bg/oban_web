6ff842cb5f0f4c4270a02fc26dbff9d856ecb17f
Parker Selbert <parker@sorentwo.com>
2021-09-15 16:23:30 -0500

    Fix transposed transport/path in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,8 @@ window.addEventListener("phx:page-loading-start", info => NProgress.start())
 window.addEventListener("phx:page-loading-stop", info => NProgress.done())
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-const transport = document.querySelector("meta[name='live-path']").getAttribute("content");
-const socketPath = document.querySelector("meta[name='live-transport']").getAttribute("content");
+const transport = document.querySelector("meta[name='live-transport']").getAttribute("content");
+const socketPath = document.querySelector("meta[name='live-path']").getAttribute("content");
 
 const liveSocket = new LiveSocket(socketPath, Socket, {
   transport: transport === "longpoll" ? LongPoll : WebSocket,
