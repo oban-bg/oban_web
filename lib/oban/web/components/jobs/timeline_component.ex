007e36a47bb4ddc0bc29af3689747fc5096c34e1
Parker Selbert <parker@sorentwo.com>
2021-12-01 10:35:23 -0600

    Label timeline components as completed
    The "attempted" language matched the timestamp, but didn't indicate the
    job's real status.

--- lib/oban/web/components/jobs/timeline_component.ex
+++ lib/oban/web/components/jobs/timeline_component.ex
@@ -25,7 +25,7 @@ defmodule Oban.Web.Jobs.TimelineComponent do
 
   def render(assigns) do
     ~H"""
-    <div class="w-1/4 flex flex-col" title={timestamp_title(@state, @job)}>
+    <div id={"timeline-for-#{@state}"} class="w-1/4 flex flex-col" data-title={timestamp_title(@state, @job)} phx-hook="Tippy">
       <span class={"flex self-center justify-center items-center h-16 w-16 transition-colors duration-200 rounded-full #{timeline_class(@state, @job)}"}>
         <%= if timeline_icon(@state, @job) == :checkmark do %>
           <svg class="h-12 w-12" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -76,7 +76,7 @@ defmodule Oban.Web.Jobs.TimelineComponent do
       {"retryable", "retryable"} -> "Retryable"
       {"retryable", _} -> "Scheduled"
       {"executing", "executing"} -> "Executing"
-      {"executing", _} -> "Attempted"
+      {"executing", _} -> "Completed"
       _ -> String.capitalize(state)
     end
   end
