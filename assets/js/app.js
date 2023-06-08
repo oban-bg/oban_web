ca52abd21ff06a215935ab27be750da90b841a2f
Parker Selbert <parker@sorentwo.com>
2023-06-08 08:11:00 -0500

    Add vertical marker plugin for line chart hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
-import "phoenix_html";
-import { Socket, LongPoll } from "phoenix";
-import { LiveSocket } from "phoenix_live_view";
-import tippy, { roundArrow } from "tippy.js";
-import topbar from "topbar";
+import "phoenix_html"
+import { Socket, LongPoll } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
+import tippy, { roundArrow } from "tippy.js"
+import topbar from "topbar"
 
 import {
   BarController,
@@ -15,57 +15,57 @@ import {
   LineElement,
   PointElement,
   Tooltip,
-} from "chart.js";
+} from "chart.js"
 
-const Hooks = {};
+const Hooks = {}
 
 // Topbar ---
 
-let topBarScheduled = undefined;
+let topBarScheduled = undefined
 
 topbar.config({
   barColors: { 0: "#0284c7" },
   shadowColor: "rgba(0, 0, 0, .3)",
-});
+})
 
 window.addEventListener("phx:page-loading-start", (info) => {
   if (!topBarScheduled) {
-    topBarScheduled = setTimeout(() => topbar.show(), 500);
+    topBarScheduled = setTimeout(() => topbar.show(), 500)
   }
-});
+})
 
 window.addEventListener("phx:page-loading-stop", (info) => {
-  clearTimeout(topBarScheduled);
-  topBarScheduled = undefined;
-  topbar.hide();
-});
+  clearTimeout(topBarScheduled)
+  topBarScheduled = undefined
+  topbar.hide()
+})
 
 // Refresher ---
 
 Hooks.Refresher = {
   mounted() {
-    const targ = "#refresh-selector";
-    const elem = this;
+    const targ = "#refresh-selector"
+    const elem = this
 
     document.addEventListener("visibilitychange", () => {
       if (document.visibilityState === "visible") {
-        elem.pushEventTo(targ, "resume-refresh", {});
+        elem.pushEventTo(targ, "resume-refresh", {})
       } else {
-        elem.pushEventTo(targ, "pause-refresh", {});
+        elem.pushEventTo(targ, "pause-refresh", {})
       }
-    });
+    })
 
     if ("refresh" in localStorage) {
-      elem.pushEventTo(targ, "select-refresh", { value: localStorage.refresh });
+      elem.pushEventTo(targ, "select-refresh", { value: localStorage.refresh })
     }
 
     this.el.querySelectorAll("[role='option']").forEach((option) => {
       option.addEventListener("click", () => {
-        localStorage.refresh = option.getAttribute("value");
-      });
-    });
+        localStorage.refresh = option.getAttribute("value")
+      })
+    })
   },
-};
+}
 
 // Theme ---
 
@@ -73,50 +73,50 @@ Hooks.RestoreTheme = {
   mounted() {
     this.pushEventTo("#theme-selector", "restore", {
       theme: localStorage.theme,
-    });
+    })
   },
-};
+}
 
 Hooks.ChangeTheme = {
   applyTheme() {
-    const wantsDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const noPreference = !("theme" in localStorage);
+    const wantsDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+    const noPreference = !("theme" in localStorage)
 
     if (
       localStorage.theme === "dark" ||
       (localStorage.theme === "system" && wantsDark) ||
       (noPreference && wantsDark)
     ) {
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add("dark")
     } else {
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove("dark")
     }
   },
 
   mounted() {
-    const elem = this;
+    const elem = this
 
     this.el.addEventListener("click", () => {
-      const theme = this.el.getAttribute("value");
+      const theme = this.el.getAttribute("value")
 
-      localStorage.theme = theme;
+      localStorage.theme = theme
 
-      this.applyTheme();
+      this.applyTheme()
 
-      elem.pushEventTo("#theme-selector", "restore", { theme: theme });
-    });
+      elem.pushEventTo("#theme-selector", "restore", { theme: theme })
+    })
   },
-};
+}
 
 // Tooltips ---
 
 Hooks.Tippy = {
   mounted() {
-    const content = this.el.getAttribute("data-title");
+    const content = this.el.getAttribute("data-title")
 
-    tippy(this.el, { arrow: roundArrow, content: content, delay: [250, null] });
+    tippy(this.el, { arrow: roundArrow, content: content, delay: [250, null] })
   },
-};
+}
 
 // Charts ---
 
@@ -130,20 +130,20 @@ Chart.register(
   LinearScale,
   PointElement,
   Tooltip,
-);
+)
 
-Chart.defaults.font.size = 12;
-Chart.defaults.font.family = "Inter var";
+Chart.defaults.font.size = 12
+Chart.defaults.font.family = "Inter var"
 
-const CYAN = "#22d3ee"; // cyan-400
-const EMERALD = "#34d399"; // emerald-400
-const ORANGE = "#fb923c"; // orange-400
-const ROSE = "#fb7185"; // rose-400
-const TEAL = "#2dd4bf"; // teal-500
-const VIOLET = "#a78bfa"; // violet-400
-const YELLOW = "#facc15"; // yellow-400
+const CYAN = "#22d3ee" // cyan-400
+const EMERALD = "#34d399" // emerald-400
+const ORANGE = "#fb923c" // orange-400
+const ROSE = "#fb7185" // rose-400
+const TEAL = "#2dd4bf" // teal-500
+const VIOLET = "#a78bfa" // violet-400
+const YELLOW = "#facc15" // yellow-400
 
-const OTHER_PALETTE = [CYAN, VIOLET, YELLOW, EMERALD, ORANGE, TEAL, ROSE];
+const OTHER_PALETTE = [CYAN, VIOLET, YELLOW, EMERALD, ORANGE, TEAL, ROSE]
 
 const STATE_PALETTE = {
   available: TEAL,
@@ -153,45 +153,79 @@ const STATE_PALETTE = {
   executing: ORANGE,
   retryable: YELLOW,
   scheduled: EMERALD,
-};
+}
 
 const estimateCount = function (value) {
   if (value < 1000) {
-    return value;
+    return value
   } else {
-    const log = Math.max(Math.floor(Math.log10(value)), 5);
-    const mult = Math.pow(10, log - 2);
-    const base = Math.floor(value / mult);
-    const part = Math.round((value % mult) / Math.pow(10, log - 3));
+    const log = Math.max(Math.floor(Math.log10(value)), 5)
+    const mult = Math.pow(10, log - 2)
+    const base = Math.floor(value / mult)
+    const part = Math.round((value % mult) / Math.pow(10, log - 3))
 
     if (part === 0) {
-      return `${base}k`;
+      return `${base}k`
     } else if (part === 10) {
-      return `${base + 1}k`;
+      return `${base + 1}k`
     } else {
-      return `${base}.${part}k`;
+      return `${base}.${part}k`
     }
   }
-};
+}
 
 const estimateNanos = function (value) {
-  const milliseconds = value / 1e6;
-  const seconds = value / 1e9;
-  const minutes = value / 6e10;
-  const hours = value / 3.6e12;
+  const milliseconds = value / 1e6
+  const seconds = value / 1e9
+  const minutes = value / 6e10
+  const hours = value / 3.6e12
 
   if (hours >= 1) {
-    return `${hours.toFixed(1)}h`;
+    return `${hours.toFixed(1)}h`
   } else if (minutes >= 1) {
-    return `${minutes.toFixed(1)}m`;
+    return `${minutes.toFixed(1)}m`
   } else if (seconds >= 1) {
-    return `${seconds.toFixed(1)}s`;
+    return `${seconds.toFixed(1)}s`
   } else if (milliseconds >= 1000) {
-    return `${milliseconds.toFixed(1)}ms`;
+    return `${milliseconds.toFixed(1)}ms`
   } else {
-    return `${milliseconds.toFixed(0)}ms`;
+    return `${milliseconds.toFixed(0)}ms`
   }
-};
+}
+
+const liner = {
+  id: "verticalLiner",
+  afterInit: (chart, args, opts) => {
+    chart.verticalLiner = {}
+  },
+  afterEvent: (chart, args, options) => {
+    const { inChartArea } = args
+    chart.verticalLiner = { draw: inChartArea }
+  },
+  beforeTooltipDraw: (chart, args, options) => {
+    const { draw } = chart.verticalLiner
+
+    if (!draw) return
+
+    const { ctx } = chart
+    const { top, bottom } = chart.chartArea
+    const { tooltip } = args
+    const x = tooltip?.caretX
+
+    if (!x) return
+
+    ctx.save()
+
+    ctx.beginPath()
+    ctx.moveTo(x, top)
+    ctx.lineTo(x, bottom)
+    ctx.strokeStyle = "#d1d5db" // gray-300
+    ctx.globalCompositeOperation = "destination-over"
+    ctx.stroke()
+
+    ctx.restore()
+  },
+}
 
 const BASIC_OPTS = {
   animation: false,
@@ -199,6 +233,7 @@ const BASIC_OPTS = {
   normalized: true,
   interaction: {
     mode: "index",
+    intersect: false,
   },
   layout: {
     padding: {
@@ -209,29 +244,30 @@ const BASIC_OPTS = {
     legend: {
       display: false,
     },
+    verticalLiner: {},
     tooltip: {
       callbacks: {
         title: function (context) {
-          const date = new Date(parseInt(context[0].label) * 1000);
+          const date = new Date(parseInt(context[0].label) * 1000)
 
-          return date.toLocaleTimeString("en-US", { hour12: false, timeStyle: "long" });
+          return date.toLocaleTimeString("en-US", { hour12: false, timeStyle: "long" })
         },
 
         label: function (context) {
-          const type = context.chart.options.type;
-          const label = context.dataset.label;
-          const value = context.parsed.y || 0;
+          const type = context.chart.options.type
+          const label = context.dataset.label
+          const value = context.parsed.y || 0
 
           if (type === "line") {
-            return `${label}: ${estimateNanos(value)}`;
+            return `${label}: ${estimateNanos(value)}`
           } else {
-            return `${label}: ${value}`;
+            return `${label}: ${value}`
           }
         },
       },
     },
   },
-};
+}
 
 const STACK_OPTS = {
   ...BASIC_OPTS,
@@ -248,9 +284,9 @@ const STACK_OPTS = {
         padding: 3,
         callback: function (value, index) {
           if (index % 4 === 0) {
-            const date = new Date(parseInt(this.getLabelForValue(value)) * 1000);
+            const date = new Date(parseInt(this.getLabelForValue(value)) * 1000)
 
-            return date.toLocaleTimeString("en-US", { hour12: false, timeStyle: "medium" });
+            return date.toLocaleTimeString("en-US", { hour12: false, timeStyle: "medium" })
           }
         },
       },
@@ -259,12 +295,12 @@ const STACK_OPTS = {
       stacked: true,
       ticks: {
         callback: function (value, index, _ticks) {
-          if (index % 2 === 0) return estimateCount(value);
+          if (index % 2 === 0) return estimateCount(value)
         },
       },
     },
   },
-};
+}
 
 const LINES_OPTS = {
   ...BASIC_OPTS,
@@ -279,9 +315,9 @@ const LINES_OPTS = {
         padding: 3,
         callback: function (value, index) {
           if (index % 4 === 0) {
-            const date = new Date(parseInt(this.getLabelForValue(value)) * 1000);
+            const date = new Date(parseInt(this.getLabelForValue(value)) * 1000)
 
-            return date.toLocaleTimeString("en-US", { hour12: false, timeStyle: "medium" });
+            return date.toLocaleTimeString("en-US", { hour12: false, timeStyle: "medium" })
           }
         },
       },
@@ -289,29 +325,30 @@ const LINES_OPTS = {
     y: {
       ticks: {
         callback: function (value, index, _ticks) {
-          if (index % 2 === 0) return estimateNanos(value);
+          if (index % 2 === 0) return estimateNanos(value)
         },
       },
     },
   },
-};
+}
 
 Hooks.Chart = {
   mounted() {
-    let chart = null;
+    let chart = null
 
     this.handleEvent("chart-change", ({ group, points, series }) => {
-      const [type, opts] = /_count/.test(series) ? ["bar", STACK_OPTS] : ["line", LINES_OPTS];
+      const [type, opts] = /_count/.test(series) ? ["bar", STACK_OPTS] : ["line", LINES_OPTS]
+      const plugins = type === "line" ? [liner] : []
 
       if (chart === null) {
-        chart = new Chart(this.el, { type: type, options: opts });
+        chart = new Chart(this.el, { type: type, options: opts, plugins: plugins })
       } else if (chart.config.type !== type) {
-        chart.destroy();
-        chart = new Chart(this.el, { type: type, options: opts });
+        chart.destroy()
+        chart = new Chart(this.el, { type: type, options: opts, plugins: plugins })
       }
 
       const datasets = Object.entries(points).map(([label, data], index) => {
-        const color = group === "state" ? STATE_PALETTE[label] : OTHER_PALETTE[index];
+        const color = group === "state" ? STATE_PALETTE[label] : OTHER_PALETTE[index]
 
         return {
           backgroundColor: color,
@@ -319,27 +356,27 @@ Hooks.Chart = {
           borderColor: color,
           data: data.reverse(),
           label: label,
-        };
-      });
+        }
+      })
 
-      chart.data.datasets = datasets;
-      chart.update();
-    });
+      chart.data.datasets = datasets
+      chart.update()
+    })
   },
-};
+}
 
 // Mounting ---
 
-const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
-const liveTran = document.querySelector("meta[name='live-transport']").getAttribute("content");
+const liveTran = document.querySelector("meta[name='live-transport']").getAttribute("content")
 
-const livePath = document.querySelector("meta[name='live-path']").getAttribute("content");
+const livePath = document.querySelector("meta[name='live-path']").getAttribute("content")
 
 const liveSocket = new LiveSocket(livePath, Socket, {
   transport: liveTran === "longpoll" ? LongPoll : WebSocket,
   params: { _csrf_token: csrfToken },
   hooks: Hooks,
-});
+})
 
-liveSocket.connect();
+liveSocket.connect()
