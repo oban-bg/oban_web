753b08adc3ada0a684ce18f1f4543f7767dc3d64
Parker Selbert <parker@sorentwo.com>
2023-06-14 09:51:53 -0500

    Destroy tippy instance when elements unmount

--- assets/js/hooks/tippy.js
+++ assets/js/hooks/tippy.js
@@ -1,10 +1,14 @@
 import tippy, { roundArrow } from "tippy.js"
 
 const Tippy = {
+  destroyed() {
+    this.tippy.destroy()
+  },
+
   mounted() {
     const content = this.el.getAttribute("data-title")
 
-    tippy(this.el, { arrow: roundArrow, content: content, delay: [250, null] })
+    this.tippy = tippy(this.el, { arrow: roundArrow, content: content, delay: [250, null] })
   },
 }
 
