a14cd6a6d777861c7d249ee1932a9b92615188d4
Parker Selbert <parker@sorentwo.com>
2023-06-19 08:05:07 -0500

    Tighten bulk action expansion transition

--- lib/oban/web/live/jobs/bulk_action_component.ex
+++ lib/oban/web/live/jobs/bulk_action_component.ex
@@ -1,6 +1,7 @@
 defmodule Oban.Web.Jobs.BulkActionComponent do
   use Oban.Web, :live_component
 
+  @impl Phoenix.LiveComponent
   def update(assigns, socket) do
     {:ok,
      assign(socket,
@@ -14,70 +15,69 @@ defmodule Oban.Web.Jobs.BulkActionComponent do
      )}
   end
 
+  @impl Phoenix.LiveComponent
   def render(assigns) do
-    expanded_class =
-      if assigns.expanded?, do: "h-12 py-3 bg-gray-100 dark:bg-gray-800", else: "h-0"
-
-    assigns = assign(assigns, expanded_class: expanded_class)
-
     ~H"""
     <div
       id="bulk-action"
-      class={"flex items-center bg-white dark:bg-gray-900 shadow-inner overflow-hidden transition-all duration-300 ease-in-out px-3 #{@expanded_class}"}
+      class={"flex items-center bg-white dark:bg-gray-900 shadow-inner overflow-hidden transition-all px-3 #{expanded_class(@expanded?)}"}
     >
-      <span class="text-sm font-semibold text-blue-500"><%= @count %> Jobs Selected</span>
+      <span class="text-sm font-semibold text-blue-500 tabular"><%= @count %> Jobs Selected</span>
 
       <%= if can?(:cancel_jobs, @access) and @cancelable? do %>
-        <a
+        <button
           id="bulk-cancel"
-          href="#"
           class="group flex items-center ml-4 text-sm text-gray-500 hover:text-blue-500"
           phx-target={@myself}
           phx-click="cancel"
+          type="button"
         >
           <Icons.x_circle class="mr-1 h-5 w-5 text-gray-400 group-hover:text-blue-500" /> Cancel
-        </a>
+        </button>
       <% end %>
 
       <%= if can?(:retry_jobs, @access) and @runnable? do %>
-        <a
+        <button
           id="bulk-retry"
-          href="#"
           class="group flex items-center ml-4 text-sm text-gray-500 hover:text-blue-500"
           phx-target={@myself}
           phx-click="retry"
+          type="button"
         >
           <Icons.arrow_right_circle class="mr-1 h-5 w-5 text-gray-400 group-hover:text-blue-500" />
           Run Now
-        </a>
+        </button>
       <% end %>
 
       <%= if can?(:retry_jobs, @access) and @retryable? do %>
-        <a
+        <button
           id="bulk-retry"
-          href="#"
           class="group flex items-center ml-4 text-sm text-gray-500 hover:text-blue-500"
           phx-target={@myself}
           phx-click="retry"
+          type="button"
         >
           <Icons.arrow_path class="mr-1 h-5 w-5 text-gray-400 group-hover:text-blue-500" /> Retry
-        </a>
+        </button>
       <% end %>
 
       <%= if can?(:delete_jobs, @access) and @deletable? do %>
-        <a
+        <button
           id="bulk-delete"
-          href="#"
           class="group flex items-center ml-4 text-sm text-gray-500 hover:text-blue-500"
           phx-target={@myself}
           phx-click="delete"
+          type="button"
         >
           <Icons.trash class="mr-1 h-5 w-5 text-gray-400 group-hover:text-blue-500" /> Delete
-        </a>
+        </button>
       <% end %>
     </div>
     """
   end
+
+  defp expanded_class(true), do: "h-12 py-3 bg-gray-100 dark:bg-gray-800"
+  defp expanded_class(_not), do: "h-0"
 
   def handle_event("cancel", _params, socket) do
     if can?(:cancel_jobs, socket.assigns.access) do
