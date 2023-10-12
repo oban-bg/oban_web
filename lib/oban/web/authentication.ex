09ad59f5a3dd6efaf4ffeb3780ac98991a13a76f
Parker Selbert <parker@sorentwo.com>
2023-10-12 14:07:34 -0500

    Mount resolvers without resolve_access/1 callback
    The `resolve_access/1` callback is optional and isn't defined on all
    resolvers.

--- lib/oban/web/authentication.ex
+++ lib/oban/web/authentication.ex
@@ -4,13 +4,14 @@ defmodule Oban.Web.Authentication do
   import Phoenix.Component
   import Phoenix.LiveView
 
-  alias Oban.Web.Telemetry
+  alias Oban.Web.{Resolver, Telemetry}
 
   def on_mount(:default, params, session, socket) do
     %{"oban" => oban, "resolver" => resolver, "user" => user} = session
 
     conf = if Oban.whereis(oban), do: Oban.config(oban), else: nil
     socket = assign(socket, conf: conf, user: user)
+    resolver = if function_exported?(resolver, :resolve_access, 1), do: resolver, else: Resolver
 
     Telemetry.action(:mount, socket, [params: params], fn ->
       case resolver.resolve_access(user) do
