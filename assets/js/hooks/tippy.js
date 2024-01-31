4072c5d15165e9a00bb0125fa2fa1a47f5a72cc7
Shannon Selbert <shannon@sorentwo.com>
2024-01-31 16:45:16 -0600

    Support updated hook in Tippy

--- assets/js/hooks/tippy.js
+++ assets/js/hooks/tippy.js
@@ -1,15 +1,21 @@
-import tippy, { roundArrow } from "tippy.js"
+import tippy, { roundArrow } from "tippy.js";
 
 const Tippy = {
   destroyed() {
-    this.tippy.destroy()
+    this.tippy.destroy();
   },
 
   mounted() {
-    const content = this.el.getAttribute("data-title")
+    const content = this.el.getAttribute("data-title");
 
-    this.tippy = tippy(this.el, { arrow: roundArrow, content: content, delay: [250, null] })
+    this.tippy = tippy(this.el, { arrow: roundArrow, content: content, delay: [250, null] });
   },
-}
 
-export default Tippy
+  updated() {
+    const content = this.el.getAttribute("data-title");
+
+    this.tippy.setContent(content);
+  },
+};
+
+export default Tippy;
