84cfbfd0a588c2b0a86c279d0f97a24111e2bff4
Kevin Schweikert <kevin@kevinschweikert.de>
2025-01-17 11:56:50 +0100

    Only show Pro version number when available

--- lib/oban/web/components/layouts.ex
+++ lib/oban/web/components/layouts.ex
@@ -164,21 +164,21 @@ defmodule Oban.Web.Layouts do
   def footer(assigns) do
     assigns =
       assign(assigns,
-        oss_version: "Oban v#{Application.spec(:oban, :vsn)}",
-        web_version: "Oban.Web v#{Application.spec(:oban_web, :vsn)}",
-        pro_version: "Oban.Pro v#{Application.spec(:oban_pro, :vsn)}"
+        oss_version: Application.spec(:oban, :vsn),
+        web_version: Application.spec(:oban_web, :vsn),
+        pro_version: Application.spec(:oban_pro, :vsn)
       )
 
     ~H"""
     <footer class="flex flex-col px-3 py-6 text-sm justify-center items-center md:flex-row">
-      <span class="text-gray-600 dark:text-gray-400 tabular mr-0 mb-1 md:mr-3 md:mb-0">
-        {@oss_version}
+      <span :if={@oss_version} class="text-gray-600 dark:text-gray-400 tabular mr-0 mb-1 md:mr-3 md:mb-0">
+        Oban v{@oss_version}
       </span>
       <span class="text-gray-600 dark:text-gray-400 tabular mr-0 mb-1 md:mr-3 md:mb-0">
-        {@web_version}
+        Oban.Web v{@web_version}
       </span>
-      <span class="text-gray-600 dark:text-gray-400 tabular mr-0 mb-3 md:mr-3 md:mb-0">
-        {@pro_version}
+      <span :if={@pro_version} class="text-gray-600 dark:text-gray-400 tabular mr-0 mb-3 md:mr-3 md:mb-0">
+        Oban.Pro v{@pro_version}
       </span>
 
       <span class="text-gray-800 dark:text-gray-200 mr-0.5 pt-1">
