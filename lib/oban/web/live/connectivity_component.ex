b143f9b1e99f7905dbfe5e77cf49754cc4e4b0d2
Parker Selbert <parker@sorentwo.com>
2025-06-09 13:27:17 -0500

    Consider metric checks for disconnected status
    The connectivity status is now determined by `Met` output as well as
    pubsub connectivity. This should make it easier to identify metric
    issues on solo nodes, e.g. in dev or a staging environment.
    
    Closes oban-bg/oban_web#127

--- lib/oban/web/live/connectivity_component.ex
+++ lib/oban/web/live/connectivity_component.ex
@@ -1,18 +1,24 @@
 defmodule Oban.Web.ConnectivityComponent do
   use Oban.Web, :live_component
 
-  alias Oban.Notifier
+  alias Oban.{Met, Notifier}
 
   @refresh :timer.seconds(15)
 
   @impl Phoenix.LiveComponent
   def update(assigns, socket) do
-    status = Notifier.status(assigns.conf)
+    checks = Met.checks(assigns.conf.name)
+
+    status =
+      assigns.conf.name
+      |> Notifier.status()
+      |> determine_status(checks)
 
     title =
       case status do
-        :isolated -> "Node is isolated, updates disabled"
-        :solitary -> "Node is solitary, not in a cluster"
+        :isolated -> "Node is isolated: Updates are disabled"
+        :solitary -> "Node is solitary: Not connected to any cluster"
+        :disconnected -> "Node is disconnected: No metrics, queues, or nodes detected"
         _ -> ""
       end
 
@@ -28,12 +34,25 @@ defmodule Oban.Web.ConnectivityComponent do
     {:ok, socket}
   end
 
+  defp determine_status(pubsub, checks) do
+    cond do
+      Enum.empty?(checks) -> :disconnected
+      pubsub == :isolated -> :isolated
+      pubsub == :solitary -> :solitary
+      true -> :connected
+    end
+  end
+
   @impl Phoenix.LiveComponent
   def render(assigns) do
     ~H"""
     <div id="connectivity" data-title={@title} phx-hook="Tippy">
       <Icons.bolt_circle :if={@status == :solitary} class="w-6 h-6 text-orange-500" />
       <Icons.bolt_slash :if={@status == :isolated} class="w-6 h-6 animate-pulse text-red-500" />
+      <Icons.exclamation_circle
+        :if={@status == :disconnected}
+        class="w-6 h-6 animate-pulse text-red-500"
+      />
     </div>
     """
   end
