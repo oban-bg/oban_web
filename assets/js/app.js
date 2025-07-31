403bba6f680962d63e4f79e0b8e499bc156d7cd9
Shannon Selbert <shannon@sorentwo.com>
2025-07-31 15:53:20 -0500

    Read phoenix js assests at compile time
    Stop bundling phoenix and liveview assests. Instead, read them at compile time and concatinate
    with app js.
    
    Solution nabbed from phoenix_live_dashboard

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,59 @@
-import "phoenix_html"
-import { Socket, LongPoll } from "phoenix"
-import { LiveSocket } from "phoenix_live_view"
-import topbar from "topbar"
+// Phoenix assets are imported from dependencies
+import topbar from "topbar";
 
-import { loadAll } from "./lib/settings"
+import { loadAll } from "./lib/settings";
 
-import Charter from "./hooks/charter"
-import Completer from "./hooks/completer"
-import Instantiator from "./hooks/instantiator"
-import Refresher from "./hooks/refresher"
-import Relativize from "./hooks/relativize"
-import Shortcuts from "./hooks/shortcuts"
-import Themer from "./hooks/themer"
-import Tippy from "./hooks/tippy"
+import Charter from "./hooks/charter";
+import Completer from "./hooks/completer";
+import Instantiator from "./hooks/instantiator";
+import Refresher from "./hooks/refresher";
+import Relativize from "./hooks/relativize";
+import Shortcuts from "./hooks/shortcuts";
+import Themer from "./hooks/themer";
+import Tippy from "./hooks/tippy";
 
-const hooks = { Charter, Completer, Instantiator, Refresher, Relativize, Shortcuts, Themer, Tippy }
+const hooks = {
+  Charter,
+  Completer,
+  Instantiator,
+  Refresher,
+  Relativize,
+  Shortcuts,
+  Themer,
+  Tippy,
+};
 
 // Topbar ---
 
-let topBarScheduled = undefined
+let topBarScheduled = undefined;
 
 topbar.config({
   barColors: { 0: "#0284c7" },
   shadowColor: "rgba(0, 0, 0, .3)",
-})
+});
 
 window.addEventListener("phx:page-loading-start", (info) => {
   if (!topBarScheduled) {
-    topBarScheduled = setTimeout(() => topbar.show(), 500)
+    topBarScheduled = setTimeout(() => topbar.show(), 500);
   }
-})
+});
 
 window.addEventListener("phx:page-loading-stop", (info) => {
-  clearTimeout(topBarScheduled)
-  topBarScheduled = undefined
-  topbar.hide()
-})
+  clearTimeout(topBarScheduled);
+  topBarScheduled = undefined;
+  topbar.hide();
+});
 
 // Mounting ---
 
-const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-const liveTran = document.querySelector("meta[name='live-transport']").getAttribute("content")
-const livePath = document.querySelector("meta[name='live-path']").getAttribute("content")
+const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+const liveTran = document.querySelector("meta[name='live-transport']").getAttribute("content");
+const livePath = document.querySelector("meta[name='live-path']").getAttribute("content");
 
-const liveSocket = new LiveSocket(livePath, Socket, {
-  transport: liveTran === "longpoll" ? LongPoll : WebSocket,
+const liveSocket = new LiveView.LiveSocket(livePath, Phoenix.Socket, {
+  transport: liveTran === "longpoll" ? Phoenix.LongPoll : WebSocket,
   params: { _csrf_token: csrfToken, init_state: loadAll() },
   hooks: hooks,
-})
+});
 
-liveSocket.connect()
+liveSocket.connect();
