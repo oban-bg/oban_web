6cb0a5c1d3a36788109ec0ed2dd32416022d4b16
Parker Selbert <parker@sorentwo.com>
2025-09-22 14:42:42 +0100

    Updated crontab row styling and data display

--- lib/oban/web/pages/crons_page.ex
+++ lib/oban/web/pages/crons_page.ex
@@ -25,23 +25,29 @@ defmodule Oban.Web.CronsPage do
           </div>
         </div>
 
-        <div
-          id="crons-table"
-          class="p-3 min-w-full grid grid-cols-[minmax(0,2.5fr)_minmax(0,0.75fr)_minmax(0,1.5fr)_minmax(0,0.2fr)] gap-y-6"
-        >
-          <ul class="contents text-xs py-1.5 font-medium uppercase tracking-wider text-gray-400 dark:text-gray-600 border-b border-gray-200 dark:border-gray-700">
-            <li>Name</li>
-            <li>Schedule</li>
-            <li>Activity</li>
-            <li>Status</li>
+        <div id="crons-table" class="min-w-full">
+          <ul class="flex items-center border-b border-gray-200 dark:border-gray-700 text-gray-400 dark:text-gray-600">
+            <.header label="name" class="ml-12 w-1/3 text-left" />
+            <div class="ml-auto flex items-center space-x-6">
+              <.header label="schedule" class="w-32 text-right" />
+              <.header label="activity" class="w-24 text-right" />
+              <.header label="status" class="w-20 pr-3 text-right" />
+            </div>
           </ul>
 
-          <ul class="contents pt-6">
+          <div :if={Enum.empty?(@crontab)} class="text-lg text-center py-12">
+            <div class="flex items-center justify-center space-x-2 text-gray-600 dark:text-gray-300">
+              <Icons.no_symbol /> <span>No crons are configured.</span>
+            </div>
+          </div>
+
+          <ul class="divide-y divide-gray-100 dark:divide-gray-800">
             <.cron_row
               :for={{expr, worker, opts} <- @crontab}
               expr={expr}
               worker={worker}
               opts={opts}
+              myself={@myself}
             />
           </ul>
         </div>
@@ -50,31 +56,82 @@ defmodule Oban.Web.CronsPage do
     """
   end
 
+  attr :label, :string, required: true
+  attr :class, :string, default: ""
+
+  defp header(assigns) do
+    ~H"""
+    <span class={[@class, "text-xs font-medium uppercase tracking-wider py-1.5 pl-4"]}>
+      {@label}
+    </span>
+    """
+  end
+
   attr :expr, :string
   attr :worker, :string
   attr :opts, :map
+  attr :myself, :any
 
   defp cron_row(assigns) do
     ~H"""
-    <li class="contents border-y-1 border-gray-100 dark:border-gray-800 hover:text-red-200">
-      <div class="font-semibold text-gray-700 dark:text-gray-300">{@worker}</div>
-      <div class="font-mono text-sm">{@expr}</div>
-      <div class=""></div>
-      <div class=""></div>
+    <li
+      id={"cron-#{cron_name(@worker, @opts)}"}
+      class="flex items-center hover:bg-gray-50 dark:hover:bg-gray-950/30"
+    >
+      <Core.row_checkbox
+        click="toggle-select"
+        value={@worker}
+        checked={false}
+        myself={@myself}
+      />
+
+      <div class="py-2.5 flex flex-grow items-center">
+        <div class="w-1/3">
+          <span class="block font-semibold text-sm text-gray-700 dark:text-gray-300">
+            {@worker}
+          </span>
+
+          <samp class="font-mono truncate text-xs text-gray-500 dark:text-gray-400">
+            {format_opts(@opts)}
+          </samp>
+        </div>
+
+        <div class="ml-auto flex items-center space-x-6 tabular text-gray-500 dark:text-gray-300">
+          <span class="w-32 text-right font-mono text-sm">
+            {@expr}
+          </span>
+
+          <span class="w-24 text-right">
+            -
+          </span>
+
+          <div class="w-20 pr-3 flex justify-end items-center space-x-1">
+            <span class="py-1.5 px-2 text-xs rounded-md bg-gray-100 dark:bg-gray-950">
+              Active
+            </span>
+          </div>
+        </div>
+      </div>
     </li>
     """
   end
 
+  defp cron_name(worker, opts) do
+    base = String.replace(worker, ".", "-")
+    hash = :erlang.phash2(opts)
+
+    "#{base}-#{hash}"
+  end
+
   @impl Page
   def handle_mount(socket) do
-    crontab = Oban.Met.crontab()
 
-    assign(socket, crontab: crontab)
+    assign_new(socket, :crontab, fn -> crontab(socket.assigns.conf) end)
   end
 
   @impl Page
   def handle_refresh(socket) do
-    socket
+    assign(socket, crontab: crontab(socket.assigns.conf))
   end
 
   @impl Page
@@ -82,8 +139,30 @@ defmodule Oban.Web.CronsPage do
     {:noreply, socket}
   end
 
+  @impl Phoenix.LiveComponent
+  def handle_event("toggle-select", %{"id" => worker}, socket) do
+    send(self(), {:toggle_select, worker})
+
+    {:noreply, socket}
+  end
+
   @impl Page
+  def handle_info({:toggle_select, _worker}, socket) do
+    # TODO: Implement cron selection logic
+    {:noreply, socket}
+  end
+
   def handle_info(_event, socket) do
     {:noreply, socket}
+  end
+
+  defp crontab(conf), do: Oban.Met.crontab(conf.name)
+
+  defp format_opts(opts) when map_size(opts) == 0, do: "%{}"
+
+  defp format_opts(opts) do
+    opts
+    |> inspect(charlists: :as_lists, limit: :infinity)
+    |> truncate(0..98)
   end
 end
