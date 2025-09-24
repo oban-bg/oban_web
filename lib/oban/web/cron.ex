bf96a4d6c4f186c5a7a90b7b0eceb413060b4acc
Parker Selbert <parker@sorentwo.com>
2025-09-24 15:14:51 +0100

    Pull static and dynamic cron with stored names

--- lib/oban/web/cron.ex
+++ lib/oban/web/cron.ex
@@ -1,12 +1,5 @@
 defmodule Oban.Web.Cron do
   @moduledoc false
 
-  defstruct [:expression, :worker, :opts, :next_at, :last_at, :last_state]
-
-  def name(%__MODULE__{worker: worker, opts: opts}) do
-    base = String.replace(worker, ".", "-")
-    hash = :erlang.phash2(opts)
-
-    "#{base}-#{hash}"
-  end
+  defstruct [:name, :expression, :worker, :opts, :next_at, :last_at, :last_state, dynamic?: false]
 end
