e7435f94cb6a57a41921efb394f94876d5f7bedb
Parker Selbert <parker@sorentwo.com>
2025-10-15 09:44:07 +0100

    Skip Oban.Pro.Migration when missing as well

--- test/support/postgres/migrations/20241028193333_add_oban_pro.exs
+++ test/support/postgres/migrations/20241028193333_add_oban_pro.exs
@@ -1,13 +1,19 @@
 defmodule Oban.Web.Repo.Migrations.AddObanPro do
   use Ecto.Migration
 
+  @compile {:no_warn_undefined, Oban.Pro.Migration}
+
   def up do
-    Oban.Pro.Migration.up()
-    Oban.Pro.Migration.up(prefix: "private")
+    if Code.ensure_loaded?(Oban.Pro.Migration) do
+      Oban.Pro.Migration.up()
+      Oban.Pro.Migration.up(prefix: "private")
+    end
   end
 
   def down do
-    Oban.Pro.Migration.down()
-    Oban.Pro.Migration.down(prefix: "private")
+    if Code.ensure_loaded?(Oban.Pro.Migration) do
+      Oban.Pro.Migration.down()
+      Oban.Pro.Migration.down(prefix: "private")
+    end
   end
 end
