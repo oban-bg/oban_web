77202a16aa41486809a8d81bde068bc254df09d4
Adrien Cohen <18153974+adriencohen@users.noreply.github.com>
2025-10-23 13:53:42 +0200

    Add queues to metadata of telemetry events

--- lib/oban/web/pages/queues_page.ex
+++ lib/oban/web/pages/queues_page.ex
@@ -263,8 +263,10 @@ defmodule Oban.Web.QueuesPage do
   def handle_info(:pause_queues, socket) do
     enforce_access!(:pause_queues, socket.assigns.access)
 
-    Telemetry.action(:pause_queues, socket, [], fn ->
-      Enum.each(socket.assigns.selected, &Oban.pause_queue(socket.assigns.conf.name, queue: &1))
+    queues = socket.assigns.selected
+
+    Telemetry.action(:pause_queues, socket, [queues: queues], fn ->
+      Enum.each(queues, &Oban.pause_queue(socket.assigns.conf.name, queue: &1))
     end)
 
     socket =
@@ -278,8 +280,10 @@ defmodule Oban.Web.QueuesPage do
   def handle_info(:resume_queues, socket) do
     enforce_access!(:pause_queues, socket.assigns.access)
 
-    Telemetry.action(:resume_queues, socket, [], fn ->
-      Enum.each(socket.assigns.selected, &Oban.resume_queue(socket.assigns.conf.name, queue: &1))
+    queues = socket.assigns.selected
+
+    Telemetry.action(:resume_queues, socket, [queues: queues], fn ->
+      Enum.each(queues, &Oban.resume_queue(socket.assigns.conf.name, queue: &1))
     end)
 
     socket =
@@ -293,8 +297,10 @@ defmodule Oban.Web.QueuesPage do
   def handle_info(:stop_queues, socket) do
     enforce_access!(:stop_queues, socket.assigns.access)
 
-    Telemetry.action(:stop_queues, socket, [], fn ->
-      Enum.each(socket.assigns.selected, &Oban.stop_queue(socket.assigns.conf.name, queue: &1))
+    queues = socket.assigns.selected
+
+    Telemetry.action(:stop_queues, socket, [queues: queues], fn ->
+      Enum.each(queues, &Oban.stop_queue(socket.assigns.conf.name, queue: &1))
     end)
 
     socket =
