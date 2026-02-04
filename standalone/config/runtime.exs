232cd37340867c6047a79e1deb5b3e279c3a46a6
Parker Selbert <parker@sorentwo.com>
2026-02-04 20:50:16 +0000

    Initialize standalone dashboard within dockerfile

---
+++ standalone/config/runtime.exs
@@ -0,0 +1,20 @@
+import Config
+
+port = String.to_integer(System.get_env("PORT", "4000"))
+
+config :oban_dashboard, ObanDashboard.Repo,
+  url: System.fetch_env!("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE", "5")),
+  show_sensitive_data_on_connection_error: true
+
+config :oban_dashboard, ObanDashboard.Endpoint,
+  http: [port: port, ip: {0, 0, 0, 0}],
+  url: [host: System.get_env("HOST", "localhost")],
+  secret_key_base: "eGKAe6cMdfeEDqQcw8LVQOExX/dWMfGIc3Ti4Pj+m5Hikugq7GdHIJWV8NAqrlr1",
+  server: true
+
+config :oban_dashboard,
+  oban_prefix: System.get_env("OBAN_PREFIX", "public"),
+  read_only: System.get_env("OBAN_READ_ONLY", "false") == "true"
+
+config :logger, level: String.to_existing_atom(System.get_env("LOG_LEVEL", "info"))
---
+++ standalone/mix.exs
@@ -0,0 +1,51 @@
+defmodule ObanDashboard.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :oban_dashboard,
+      version: "0.1.0",
+      elixir: "~> 1.19",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      releases: releases()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger, :runtime_tools],
+      mod: {ObanDashboard.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:phoenix, "~> 1.7"},
+      {:phoenix_live_view, "~> 1.0"},
+      {:phoenix_html, "~> 4.0"},
+      {:bandit, "~> 1.5"},
+      {:ecto_sql, "~> 3.10"},
+      {:postgrex, "~> 0.19"},
+      {:oban, "~> 2.20"},
+      {:oban_web, "~> 2.11"},
+      {:oban_met, "~> 1.0"}
+    ] ++ oban_pro_dep()
+  end
+
+  defp oban_pro_dep do
+    if System.get_env("OBAN_LICENSE") do
+      [{:oban_pro, "~> 1.6", repo: :oban}]
+    else
+      []
+    end
+  end
+
+  defp releases do
+    [
+      oban_dashboard: [
+        include_executables_for: [:unix]
+      ]
+    ]
+  end
+end
