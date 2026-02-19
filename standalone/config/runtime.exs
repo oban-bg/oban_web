0156b53e57a6ffea2155eca3b97de53a9a2e1a7e
Paolo Quadri <paolo.quadri@remote.com>
2026-02-19 19:13:19 +0100

    feat: add ssl verify option from env

--- standalone/config/runtime.exs
+++ standalone/config/runtime.exs
@@ -5,18 +5,12 @@ port = String.to_integer(System.get_env("PORT", "4000"))
 config :oban_dash, ObanDash.Repo,
   url: System.fetch_env!("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE", "5")),
-  show_sensitive_data_on_connection_error: true
+  show_sensitive_data_on_connection_error: true,
+  ssl: String.to_existing_atom(System.get_env("SSL", "false")) in ["true", "1"],
+  ssl_opts: [verify_mode: String.to_existing_atom(System.get_env("SSL_VERIFY_MODE", "verify_peer"))]
 
 config :oban_dash, ObanDash.Endpoint,
   http: [port: port, ip: {0, 0, 0, 0}],
   url: [host: System.get_env("HOST", "localhost")],
   secret_key_base: "eGKAe6cMdfeEDqQcw8LVQOExX/dWMfGIc3Ti4Pj+m5Hikugq7GdHIJWV8NAqrlr1",
   server: true
-
-config :oban_dash,
-  oban_prefix: System.get_env("OBAN_PREFIX", "public"),
-  read_only: System.get_env("OBAN_READ_ONLY", "false") == "true",
-  basic_auth_user: System.get_env("BASIC_AUTH_USER"),
-  basic_auth_pass: System.get_env("BASIC_AUTH_PASS")
-
-config :logger, level: String.to_existing_atom(System.get_env("LOG_LEVEL", "info"))
